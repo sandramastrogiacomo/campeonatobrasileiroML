1096725c966ad584d94d7fdc1b0867930c276c22
Sandra Mastrogiacomo <sandramastrogiacomo@hotmail.com>
2025-06-11 11:36:58 -0300

    "estrutura inicial do projeto"

---
+++ src/main/java/com/campeonatobrasileiro/brasileirao_api/CampeonatoBrasileiroApiApplication.java
@@ -0,0 +1,13 @@
+package com.campeonatobrasileiro.brasileirao_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampeonatoBrasileiroApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampeonatoBrasileiroApiApplication.class, args);
+	}
+
+}
