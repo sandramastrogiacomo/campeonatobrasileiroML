451bbe3fb54378f298d054d15323ea00f367f25f
Sandra Mastrogiacomo <sandramastrogiacomo@hotmail.com>
2025-06-30 22:58:08 -0300

    inicia implementacao de buscas avancadas

---
+++ src/main/java/com/campeonatobrasileiro/brasileirao_api/dto/ClubStatsResponseDTO.java
@@ -0,0 +1,25 @@
+package com.campeonatobrasileiro.brasileirao_api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ClubStatsResponseDTO {
+    private Long clubId;
+    private String clubName;
+    private int gamesPlayed;
+    private int gamesWon;
+    private int gamesLost;
+    private int gamesDraw;
+    private int goalsScored;
+    private int goalsConceded;
+    private int goalsDifference;
+    private int points;
+
+
+}
